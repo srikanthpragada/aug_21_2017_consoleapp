d35a7d6d3975ba21f1d06e303790877587c5174d
srikanthpragada <srikanthpragada@gmail.com>
2017-08-31 09:29:51 +0530

    31-AUG

---
+++ DateDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class DateDemo
+    {
+        public static void Main()
+        {
+            DateTime ct = DateTime.Now;
+            DateTime et = ct.AddDays(30);
+
+            Console.WriteLine(et);
+
+            TimeSpan  period = et.Subtract(ct);
+
+            Console.WriteLine(period.Days);
+
+            DateTime boy = DateTime.Parse("1/1/2017");
+            TimeSpan days = ct.Subtract(boy);
+            Console.WriteLine(days.ToString());
+                
+
+
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Person : IComparable<Person>
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+
+        // 0   this == other 
+        // >0  this > other
+        public int CompareTo(Person other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+            return this.Name == other.Name && this.Age == other.Age;
+        }
+
+        public override string ToString()
+        {
+            return Name + " : " + Age;
+        }
+    }
+
+    class TestPerson
+    {
+        public static void Main()
+        {
+            Person[] people = {
+                new Person { Name = "Elon Musk", Age = 40 },
+                new Person { Name = "Bill Gates", Age = 60 },
+                new Person { Name = "Larry Ellison", Age = 65 },
+                new Person { Name = "Andy Gore", Age = 55 }
+            };
+
+
+            foreach (Person p in people)
+                Console.WriteLine(p);  // p.ToString()
+
+            Array.Sort(people);
+
+
+            foreach (Person p in people)
+                Console.WriteLine(p);  // p.ToString()
+        }
+
+    }
+}
