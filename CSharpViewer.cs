bc0d355ff29979d4cf23f22d18cc365bed7e0564
srikanthpragada <srikanthpragada@gmail.com>
2017-09-01 09:30:39 +0530

    1-SEP

---
+++ CSharpViewer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class CSharpViewer
+    {
+        public static void Main(string [] args)
+        {
+            string dir = @"f:\classroom\aug22\consoleapp";  // default folder = 
+
+            if (args.Length > 0)
+                dir = args[0];
+
+            DirectoryInfo dinfo = new DirectoryInfo(dir);
+
+            foreach(FileInfo  finfo in  dinfo.GetFiles() )
+            {
+                if (finfo.FullName.EndsWith(".cs"))
+                    PrintFile(finfo.FullName);
+            }
+        }
+
+        public static void PrintFile(string filename)
+        {
+            Console.WriteLine("\n=================== {0} ==================\n", filename);
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                int lineno = 1;
+                while (true)
+                {
+                    string line = sr.ReadLine();
+                    if (line == null)
+                        break;
+                    Console.WriteLine("{0:000}: {1}",lineno++, line);
+                }
+            }
+
+        }
+    }
+}
