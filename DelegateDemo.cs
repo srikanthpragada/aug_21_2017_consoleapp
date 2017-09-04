cc6f4cdc7cdbfbf0225ac938a5ca7d316aa83551
srikanthpragada <srikanthpragada@gmail.com>
2017-09-04 09:30:14 +0530

    4-SEP

---
+++ DelegateDemo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class DelegateDemo
+    {
+        delegate void PrintDelegate(string s);
+
+        static void Main(string[] args)
+        {
+            Action a1 = new Action(Print);
+            a1();
+
+            Action<string> a2 = Display;  // Method group conversion 
+            a2("Second Delegate");
+
+            Action<string> a3 =
+                delegate (string msg)
+                {
+                   Console.WriteLine(msg);
+                };
+
+            a3("Anonymous Method");
+
+            Action<string> a4 = (string s) => Console.WriteLine(s);
+            a4("Lambda Expression");
+
+            Action<string> a5 = (string s) =>
+                {
+                    for (int i = 1; i <= 5; i++)
+                        Console.WriteLine(s);
+                };
+
+            a5("Lambda");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("In Print()");
+        }
+
+        public static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Stack.cs
+++ Stack.cs
@@ -8,8 +8,26 @@ namespace ConsoleApp
 {
     class TestStack
     {
+
+        public static void Print<T> (T[] a)  where T : IComparable<T>
+        {
+            foreach (T v in a)
+                Console.WriteLine(v);
+        }
+    
         public static void Main()
         {
+            int[] nums = new int[] { 10, 20, 38, 03 };
+            Print(nums);
+
+            /*
+            Time[] times = new Time[] { new Time { Hours = 10, Mins = 20, Secs = 30 } };
+            Print(times);
+            */
+
+
+            #region old stack example
+            /*
             IntStack s = new IntStack();
             s.Push(10);
             s.Push(20);
@@ -20,14 +38,48 @@ namespace ConsoleApp
             ss.Push("First");
             ss.Push("Second");
             Console.WriteLine(ss.Pop());
+            */
+#endregion
+
+
+            var names = new Stack<string>();
+            names.Push("Abc");
+            names.Push("Xyz");
+            names.Push("Pqr");
+            Console.WriteLine(names.Pop());
+
+
+            var prices = new Stack<double>();
+            prices.Push(29.29);
+            prices.Push(19.19);
+            Console.WriteLine(prices.Pop());
+
+            var stack = new CommonStack();
+            stack.Push("String1");
+            stack.Push("String2");
+            stack.Push(10);
+            stack.Push(DateTime.Now);
+            var s = stack.Pop();
+
+            string st = s as string;
         }
     }
+
 
     // Generic Stack 
     class Stack<T>
     {
         private T[] data = new T[10];
         private int top = 0;
+
+        public void Clear()
+        {
+            for (int i = 0; i < data.Length; i++)
+                data[i] = default(T);  // Take default value of type T
+
+            top = 0; 
+
+        } 
 
         public void Push(T value)
         {
@@ -41,6 +93,22 @@ namespace ConsoleApp
         }
     }
 
+    class CommonStack
+    {
+        private  object [] data = new  object[10];
+        private int top = 0;
+
+        public void Push(object value)
+        {
+            data[top] = value;
+            top++;
+        }
+        public object Pop()
+        {
+            top--;
+            return data[top];
+        }
+    }
 
     class IntStack
     {
