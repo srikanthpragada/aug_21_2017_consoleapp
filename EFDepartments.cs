9839af8432b02fc6061d8d2d2dc7d04aaa5b7fcc
srikanthpragada <srikanthpragada@gmail.com>
2017-09-28 09:32:19 +0530

    28-SEP

--- EFDepartments.cs
+++ EFDepartments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,61 @@ namespace ConsoleApp
             // AddDepartment();
             // UpdateDepartment();
             // DeleteDepartment();
-            ListDepartments();
+            // FindThenUpdateDepartment();
+            // TwoContextProblem();
+
+            LoadTwice();
+            // ListDepartments();
+
+            
+        }
+
+        private static void LoadTwice()
+        {
+            HREntities ctx1 = new HREntities();
+            ctx1.Database.Log = Console.WriteLine;
+
+            var dept = ctx1.Departments.Find(50);
+            dept.DepartmentName = "Sportsssss";
+
+            // ctx1.Entry(dept).State = EntityState.Detached;
+
+            var dept2 = ctx1.Departments.Find(50);
+            Console.WriteLine(dept2.DepartmentName);
+
+
+            Console.WriteLine(dept == dept2);
+
+        }
+
+        private static void TwoContextProblem()
+        {
+            HREntities ctx1 = new HREntities();
+            ctx1.Database.Log = Console.WriteLine;
+
+            var dept = ctx1.Departments.Find(50);
+            Console.WriteLine("Before change {0}", ctx1.Entry(dept).State);
+
+            dept.DepartmentName = "New Sports";
+
+            Console.WriteLine("After change {0}", ctx1.Entry(dept).State);
+
+            ctx1.Dispose();
+
+            // dept is detached 
+
+            Console.WriteLine("First context is closed!");
+
+            HREntities ctx2 = new HREntities();
+            ctx2.Database.Log = Console.WriteLine;
+
+            ctx2.Departments.Attach(dept);
+            Console.WriteLine("After attached {0}", ctx2.Entry(dept).State);
+            ctx2.Entry(dept).State = EntityState.Modified;
+            Console.WriteLine("After change  {0}", ctx2.Entry(dept).State);
+
+            ctx2.SaveChanges();
+
         }
 
         private static void DeleteDepartment()
@@ -35,11 +90,28 @@ namespace ConsoleApp
             }
         }
 
+        private static void FindThenUpdateDepartment()
+        {
+            HREntities ctx = new HREntities();
+            ctx.Database.Log = Console.WriteLine;
+
+            var dept = ctx.Departments.Find(50);
+            if (dept == null)
+                Console.WriteLine("Sorry! Department not found!");
+            else
+            {
+
+                dept.DepartmentName = "Sports";
+                ctx.SaveChanges();
+                Console.WriteLine("Updated Successfully!");
+            }
+        }
+
         private static void UpdateDepartment()
         {
             HREntities ctx = new HREntities();
             ctx.Database.Log = Console.WriteLine;
-            
+
 
             var dept = ctx.Departments.Where(d => d.DepartmentId == 150).SingleOrDefault();
             Console.WriteLine("Retrieved object from db");
@@ -63,12 +135,20 @@ namespace ConsoleApp
             HREntities ctx = new HREntities();
             ctx.Database.Log = Console.WriteLine;
 
-            Department dept = new Department { DepartmentId = 150,
-                                               DepartmentName = "Customer Support" };
+            Department dept = new Department
+            {
+                DepartmentId = 150,
+                DepartmentName = "Customer Support"
+            };
+
+            // dept is detached 
 
             ctx.Departments.Add(dept);
+
+            // dept is added 
+
             Console.ReadLine();
-            ctx.SaveChanges();
+            ctx.SaveChanges(); // Insert 
             Console.WriteLine("Added Dept!");
         }
 
@@ -78,10 +158,10 @@ namespace ConsoleApp
             HREntities ctx = new HREntities();
             ctx.Database.Log = Console.WriteLine;
 
-            var depts = from dept in ctx.Departments.Include("Employees")
-                        select dept; 
+            var depts = from dept in ctx.Departments
+                        select dept;
 
-            foreach (Department dept in  depts)
+            foreach (Department dept in depts)
             {
                 Console.WriteLine("{0} - {1}",
                     dept.DepartmentId, dept.DepartmentName);
